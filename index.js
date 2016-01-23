6567cde0644ece9b67741614168a609c608f33a9
Engin Arslan <hibernationtheory@gmail.com>
2016-01-23 18:49:25 -0500

    rename config file and update package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var casper = require('casper').create();
 var main = require('./main.js');
 
-main('./casperConfig.json').then(function(results) {
+main('./config.json').then(function(results) {
 	console.log(JSON.stringify(results));
 	casper.exit();
 });
