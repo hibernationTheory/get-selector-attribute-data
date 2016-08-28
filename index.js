09ccc5eddf872e24945d28ba80d3ff92335b3f98
Engin Arslan <hibernationtheory@gmail.com>
2016-08-28 00:29:41 -0400

    fix indentation

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var casper = require('casper').create();
-// uncomment below for outputting the data to disk
-//var fs = require('fs');
 var main = require('./main.js');
 var fs = require('fs');
 
 main('./config.json', './config_searchSelection.json')
 	.then(function(results) {
-	var results = JSON.stringify(results, null, '\t');
-	var outputFileName ='output.json';
-	console.log(results);
-	fs.write(outputFileName, results);
-	casper.exit();
+		var results = JSON.stringify(results, null, '\t');
+		var outputFileName ='output.json';
+		console.log(results);
+		fs.write(outputFileName, results);
+		casper.exit();
 });
